34ef64e6331ab10d44c306ed1f2a1bfd707e6636
Matthew Berryman <mberryma@uow.edu.au>
2014-12-09 14:31:47 +1100

    Make cleanup a function

--- clean_up.py
+++ clean_up.py
@@ -2,24 +2,23 @@
 
 import re
 
-training_set = set()
-
-test_set = set()
-
-for line in open('training_2.txt','r'):
+def cleanup_input_data(training_filename, test_filename):
+  training_set = set()
+  test_set = set()
+  for line in open(training_filename,'r'):
     line = line.rstrip()
     training_set.add(line)
 
-for line in open('test.txt','r'):
+  for line in open(test_filename,'r'):
     if (line.rstrip() not in training_set) and (line.rstrip().endswith(',')):
-        test_set.add(line.rstrip())
+      test_set.add(line.rstrip())
 
-training = open('training_2_cleaned.txt','w')
-test = open('test_cleaned.txt','w')
+  training = open(training_filename[0:-4]+'_cleaned.txt','w')
+  test = open(test_filename[0:-4]+'_cleaned.txt','w')
 
-for line in training_set:
+  for line in training_set:
     print(line,file=training)
 
-for line in test_set:
+  for line in test_set:
     if (not re.search('Credit:',line)) and (not re.search('Title:',line)):
-        print(line,file=test)
+      print(line,file=test)
