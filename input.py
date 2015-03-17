2762463483459d0162b56d3e473dd21860b4b285
Matthew Berryman <mberryma@uow.edu.au>
2015-03-17 16:28:11 +1100

    Fix storage & change example
    1. Fix data store to make sure every article is stored only once.
    2. For example code, split into test & training data.

--- input.py
+++ input.py
@@ -20,6 +20,8 @@ def load_set(filenames): # for loading a test or training set
 
 
 def read_anzns(filename):
+  title = ''
+  seen = set()
   storing = False
   fulltext = ''
   year = ''
@@ -40,6 +42,9 @@ def read_anzns(filename):
 #      storing = False
 #      fulltext = ''
 
+    if line.startswith('Title:'):
+      title = line[7:]
+
     if line.startswith('Publication year:'):
       year = line[18:]
 
@@ -47,21 +52,25 @@ def read_anzns(filename):
       location = line[22:]
 
     if len(fulltext) > 1 and not storing:
-        print(location)
-        by_year = {}
-        try:
-          by_year = store[location]
-        except KeyError:
-          store[location] = {}
-        try:
-          by_year[year].append(fulltext)
-        except KeyError:
-          by_year[year] = [fulltext]
-        store[location] = by_year
+        if title not in seen:
+          print(location)
+          by_year = {}
+          try:
+            by_year = store[location]
+          except KeyError:
+            store[location] = {}
+          try:
+            by_year[year].append(fulltext)
+          except KeyError:
+            by_year[year] = [fulltext]
+          store[location] = by_year
+          fulltext = ''
+        else:
+          seen.add(title)
         fulltext = ''
   return store
 
-
+# Next function needs further testing.
 def factiva(filename):
   storing = False
   dateLineNext = False
@@ -102,10 +111,11 @@ if __name__ == '__main__':
   other_articles = []
 
   wollongong_test = open('wollongong_test.txt','w')
-  wollongong_training = open('wollongong_training','w')
+  wollongong_training = open('wollongong_training.txt','w')
   kiama_test = open('kiama_test.txt','w')
   kiama_training = open('kiama_training.txt','w')
   other_test = open('other_test.txt','w')
+  other_training = open('other_training.txt','w')
 
   for location in articles.keys():
     if location.startswith('Wollongong'):
@@ -118,21 +128,30 @@ if __name__ == '__main__':
       for article in [item for sublist in articles[location].values() for item in sublist]:
         other_articles.append(article)
 
-  for i in range(min(len(wollongong_articles),88)):
+  for i in range(88):
     wollongong_article = TextBlob(random.choice(wollongong_articles))
     for sentence in wollongong_article.sentences:
       if 'NBN' in sentence:
-        wollongong_text.write(str(sentence) + ',\n')
+        if i < 44:
+          wollongong_test.write(str(sentence) + ',\n')
+        if i > 44:
+          wollongong_training.write(str(sentence) + ',\n')
 
   for i in range(min(len(kiama_articles),88)):
     print(str(i))
     kiama_article = TextBlob(kiama_articles[i])
     for sentence in kiama_article.sentences:
       if 'NBN' in sentence:
-        kiama_text.write(str(sentence) + ',\n')
+        if i < 44:
+          kiama_test.write(str(sentence) + ',\n')
+        if i > 44:
+          kiama_training.write(str(sentence) + ',\n')
 
   for i in range(min(len(other_articles),88)):
     other_article = TextBlob(random.choice(other_articles))
     for sentence in other_article.raw_sentences:
       if 'NBN' in sentence:
-        other_text.write(str(sentence) + ',\n')
+        if i < 44:
+          other_test.write(str(sentence) + ',\n')
+        if i > 44:
+          other_training.write(str(sentence) + ',\n')
